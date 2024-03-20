a4b31beb572c7e8d195ade2dbc3375ad085e8762
Abinash Kalita <abinashkalita682@gmail.com>
2024-03-21 00:21:57 +0530

    projSec restructure

--- script.js
+++ script.js
@@ -1,53 +1,33 @@
-var locoScroll;
-var crsr= document.querySelector("#cursor")
-var crsrBG= document.querySelector("#cursor-bg")
+(function () {
+   const locomotiveScroll = new LocomotiveScroll();
+})();
 
 
 
-function init() {
-   gsap.registerPlugin(ScrollTrigger);
+var crsr = document.querySelector("#cursor")
+var crsrBG = document.querySelector("#cursor-bg")
 
-   locoScroll = new LocomotiveScroll({
-      el: document.querySelector("#main"),
-      smooth: true,
-      multiplier: 0.2,
-   });
-   locoScroll.on("scroll", ScrollTrigger.update);
 
-   ScrollTrigger.scrollerProxy("#main", {
-      scrollTop(value) {
-         return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-      },
-      getBoundingClientRect() {
-         return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
-      },
-      pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
-   });
 
-   ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-   ScrollTrigger.refresh();
+function cursorMoveAnimation() {
+   document.addEventListener("mousemove", function (dets) {
+      crsr.style.left = dets.x + 12 + "px"
+      crsr.style.top = dets.y - 5 + "px"
+      crsrBG.style.left = dets.x + "px"
+      crsrBG.style.top = dets.y + "px"
+   })
 
 }
 
-init()
 
-
-document.addEventListener("mousemove", function(dets){
-   crsr.style.left= dets.x+12 + "px"
-   crsr.style.top= dets.y-5 + "px"
-   crsrBG.style.left= dets.x + "px"
-   crsrBG.style.top= dets.y + "px"
-})
-
-function crsrHov(elem){
-    elem.addEventListener("mouseenter", ()=>{
+function crsrHov(elem) {
+   elem.addEventListener("mouseenter", () => {
       crsrBG.style.scale = 0
       crsr.style.scale = 2
       crsr.style.border = "1px solid white"
       crsr.style.backgroundColor = "transparent"
    })
-   elem.addEventListener("mouseleave", ()=>{
+   elem.addEventListener("mouseleave", () => {
       crsrBG.style.scale = 1
       crsr.style.scale = 1
       crsr.style.border = "none"
@@ -55,330 +35,266 @@ function crsrHov(elem){
    })
 }
 
-var btns=document.querySelectorAll(".btn")
-var navlinks=document.querySelectorAll("#nav .navLinks .listItem")
-btns.forEach((btn)=>{
-   crsrHov(btn)
-})
-navlinks.forEach((link)=>{
-   crsrHov(link)
 
-   console.log('djhsfjsdhf');
+function autoScroll() {
    
-   var linkurlID=link.dataset.hrefsecId
-   var inner1stElem= link.dataset.inner1stClass
-   
-   link.addEventListener("click",()=>{
-      if(inner1stElem ==""){
+   var navlinks = document.querySelectorAll("#nav .navLinks .listItem")
+   navlinks.forEach((link) => {
+      crsrHov(link)
+
+
+      var linkurlID = link.dataset.scrollto
+
+      link.addEventListener("click", () => {
          var elem = document.querySelector(`#${linkurlID}`)
-      }
-      else{
-         var elem = document.querySelector(`#${linkurlID} .${inner1stElem}`)
-      }
-      locoScroll.scrollTo(elem);
-   })
-})
 
+         if (linkurlID == "home") {
+            console.log('fgio');
 
-gsap.from("#part1 h1,#part1 h4,#part1 h2",{
-   x: -100,
-   opacity:0,
-   letterSpacing: "1.5rem",
-   fontSize: "2rem",
-   stagger: .3,
-   delay:.3,
-   duration:.5,
-})
-
-gsap.from("#i-dot",{
-   x: 100,
-   width: 100,
-   ease: 'power2.out',
-   opacity:0,
-   delay:1,
-   duration:.7,
-})
-
-gsap.from("#part1 .right",{
-   scale:0,
-   opacity:0,
-   stagger: .5,
-   delay:1.3,
-   duration:.5,
-})
-
-gsap.from("#scroll",{
-   y: 100,
-   opacity:0,
-   delay:1.5,
-   duration:.3,
-})
-
-
-
-
-gsap.to("#scroll",{
-   bottom: "8%",
-   repeat: -1,
-   duration: 1,
-   yoyo: true,
-})
-
-
-
-
-var tl1 = gsap.timeline({
-   scrollTrigger: {
-      trigger: "#homeSec",
-      scroller: "#main",
-      start: "top 0",
-      end: "bottom 0",
-      scrub: 2,
-      pin: true,
-      onUpdate: function(elem){
-         if(elem.progress > 0.6){
-            document.querySelector("#about").style.display= "flex"
-         }
-         else{
-            document.querySelector("#about").style.display= "none"
-         }
-      }
-   }
-})
-
-
-tl1.to("#i-dot", {
-   rotate: 180,
-   left: "50%",
-   top: "50%",
-   scale:3
-})
-
-tl1.to("#i-dot", {
-   scale: 60,
-   rotate: 360,
-   backgroundColor: "#c3c1c1",
-})
-
-tl1.from("#homeSec #about,#nav", {
-   display:"none",
-   opacity: 0,
-   duration: .5,
-},"part2Elem")
-
-
-tl1.from("#homeSec #about .cont1 img", {
-   x: -100,
-   opacity: 0,
-   duration: .5,
-},"part2Elem")
-
-tl1.from("#homeSec #about .cont1 .right", {
-   x: 100,
-   opacity: 0,
-   duration: .5,
-},"part2Elem")
-
-
-
-
-
-
-function excNavHeight() {
-   var wHeight=window.innerHeight -(window.innerHeight-70)
-   return wHeight
-}
-
-
-function assign_zIndex(elemList){
-   elemList.forEach(function (elem,index){
-      var zIndex= imgCard.length -index
-      elem.style.zIndex = zIndex 
-   });
-}
-
-
-function assignLeftValue(elemList, start, difference) {
-   imgCard.forEach(function (elem, index) {
-      if (index <= 3) {     
-         elem.style.left = start-difference*index +"%"  
-      }
-      else {
-         elem.style.left = start+"%"      
-      }
-   });   
-}
-
-
-var  imgCard= document.querySelectorAll("#projectsSec .cont1 .imgCard")
-var  textCard= document.querySelectorAll("#projectsSec .cont2 .textCard")
-assign_zIndex(imgCard)
-assign_zIndex(textCard)
-assignLeftValue(imgCard, 15, 3)
-
-
-
-
-
-
-
-gsap.to("body, #about,#nav",{
-   backgroundColor: "black",
-   color: "crimson",
-   scrollTrigger:{
-      trigger:"#projectsSec",
-      scroller: "#main",
-      start: "top 30%",
-      end: "top 30%",
-      scrub:2,
-   }
-})
-
-
-gsap.from("#projectsSec #fProj h1",{
-   fontSize: "10rem",
-   letterSpacing: "5rem",
-   opacity: 0,
-   duration:2,
-   scrollTrigger: {
-      trigger: "#projectsSec",
-      scroller: "#main",
-      start: `top 90%`,
-      end: "top 15%",
-      scrub: 2,
-   }
-   
-})
-
-
-
-var tl2 = gsap.timeline({
-   scrollTrigger: {
-      trigger: "#projectsSec",
-      scroller: "#main",
-      start: `top 0`,
-      end: "bottom 0",
-      scrub: 2,
-      pin: true,
-   }
-})
-
-
-
-tl2.from("#projectsSec .cont1 .imgCard",{
-   left: "-100%",
-   duration:2,
-   stagger: .2,   
-},"ghd")
-
-
-
-tl2.to("#projectsSec .cont1 .imgCard",{
-   left: "20%",
-   top:"18%",
-   scale: 1.3,
-   delay: 1.5,
-   rotate: 80,
-   duration: 3,
-},"itemMove")
-
-tl2.from("#projectsSec .cont2",{
-   opacity:0,
-   y: "100%",
-   delay: 1.5,
-   duration: 3,
-},"itemMove")
-
-tl2.to("#projectsSec #fProj",{
-   left: "-4%",
-   top: "7%",
-   scale: 0.6,
-   delay: 1.5,
-   duration: 3,
-},"itemMove")
-
-
-
-tl2.to("#projectsSec .cont1 .imgCard",{
-   delay:1,
-   y: "-150%",
-   duration: 5,
-   stagger: 5,
-},"slideCardAnim")
-
-tl2.to("#projectsSec .cont2 .textCard",{
-   delay:1,
-   y: "-250%",
-   duration: 5,
-   stagger: 5,
-},"slideCardAnim")
-
-
-
-tl2.to("#projectsSec #fProj,#projectsSec #fProj h1",{
-   top: "95%",
-   left:"20%",
-   fontSize: "10rem",
-   // letterSpacing: "5rem",
-   opacity:0,
-   duration:2,
-})
-   
-
-
-
-
-var tl3= gsap.timeline({
-    scrollTrigger:{
-      trigger:"#quoteSec",
-      scroller:"#main",
-      start:"top 40%",
-      end: "top 40%",
-      scrub: 3,
-      pin: true,
-      onUpdate: (inst)=>{
-         
-         if (inst.progress > 0.8) {
-            gsap.to("#nav,body",{
-               backgroundColor:"#c3c1c1",
-               color: "black",
-               duration: 2
-            })
+            gsap.to(window, { duration: 3, scrollTo: 0 });
          }
          else {
-            gsap.to("#nav,body",{
-               backgroundColor: "black",
-               color: "crimson",
-               duration: 2
-            })
+            gsap.to(window, { duration: 3, scrollTo: { y: elem, offsetY: 50 } });
          }
+
+      })
+   })
+}
+
+
+function initialAnimation() {
+   gsap.from("#part1 h1,#part1 h4,#part1 h2", {
+      x: -100,
+      opacity: 0,
+      letterSpacing: "1.5rem",
+      fontSize: "2rem",
+      stagger: .3,
+      delay: .3,
+      duration: .5,
+   })
+
+   gsap.from("#i-dot", {
+      x: 100,
+      width: 100,
+      ease: 'power2.out',
+      opacity: 0,
+      delay: 1,
+      duration: .7,
+   })
+
+   gsap.from("#part1 .right", {
+      scale: 0,
+      opacity: 0,
+      stagger: .5,
+      delay: 1.3,
+      duration: .5,
+   })
+
+   gsap.from("#scroll", {
+      y: 100,
+      opacity: 0,
+      delay: 1.5,
+      duration: .3,
+   })
+
+
+
+   gsap.to("#scroll", {
+      bottom: "8%",
+      repeat: -1,
+      duration: 1,
+      yoyo: true,
+   })
+
+
+}
+
+
+function homeAnimation() {
+   var tl1 = gsap.timeline({
+      scrollTrigger: {
+         trigger: "#homeSec",
+         start: "top top",
+         end: "bottom 0%",
+         scrub: 2,
+         pin: true,
       }
+   })
+
+
+   tl1.to("#i-dot", {
+      rotate: 180,
+      left: "50%",
+      top: "50%",
+      scale: 2
+   })
+
+   tl1.to("#i-dot", {
+      scale: 60,
+      rotate: 360,
+      backgroundColor: "#c3c1c1",
+   })
+
+   tl1.from("#homeSec #about,#nav", {
+      display: "none",
+      opacity: 0,
+      // delay: 1,
+   }, "part2Elem")
+
+
+   tl1.from("#homeSec #about .cont1 img", {
+      x: -100,
+      opacity: 0,
+      // delay: 1,
+   }, "part2Elem")
+
+   tl1.from("#homeSec #about .cont1 .right", {
+      x: 100,
+      opacity: 0,
+      // delay: 1,
+   }, "part2Elem")
+
+
+}
+
+
+function projectsSecAnimation() {
+
+   gsap.from("#projectsSec #fProj h1", {
+      fontSize: "10rem",
+      letterSpacing: "5rem",
+      opacity: 0,
+      duration: 2,
+      scrollTrigger: {
+         trigger: "#projectsSec",
+         start: `top 90%`,
+         end: "top 15%",
+         scrub: 2,
+      }
+
+   })
+
+
+
+   var tl = gsap.timeline({
+
+      scrollTrigger: {
+         trigger: "#projectsSec ",
+         start: `top top`,
+         end: "bottom 0%",
+         scrub: 2,
+         pin: true,
+      }
+   })
+
+
+   tl.to("#projectsSec #fProj", {
+      left: "-4%",
+      top: "7%",
+      scale: 0.6,
+      duration: .2,
+   }, "aa")
+
+
+   tl.from("#projectsSec .cont", {
+      x: "100%",
+      duration: .2,
+   }, "aa")
+
+
+
+   tl.to("#projectsSec .cont", {
+      x: xPercentCount(),
+      ease: Power4,
+   })
+
+
+   function xPercentCount(elem) {
+      elem = document.querySelector("#projectsSec .cont")
+      console.log();
+      return `-${(elem.childElementCount - 1) * 100}%`
+
+
    }
+}
+
+
+function quoteAnimation() {
+
+   var tl = gsap.timeline({
+      scrollTrigger: {
+         trigger: "#quoteSec",
+         start: "top 70%",
+         end: "top 00%",
+         scrub: 3,
+      }
+   })
+
+
+   tl.from("#quoteSec .quote i:nth-child(1)", {
+      x: "-100px",
+      y: "-100px",
+      opacity: 0,
+   }, "colonAnim")
+
+
+   tl.from("#quoteSec .quote i:nth-child(3)", {
+      x: "100px",
+      y: "100px",
+      opacity: 0,
+   }, "colonAnim")
+
+}
+
+
+function themeAnimation() {
+
+   document.querySelectorAll(".cCngSec").forEach(function (el) {
+
+      var el2 = el
+      if (el.parentElement.classList.contains("pin-spacer") == true) {
+         el2 = el.parentElement
+      };
+
+
+      ScrollTrigger.create({
+         trigger: el2,
+         start: "top 50%",
+         end: "bottom 50%",
+         scrub: 2,
+
+         onEnter: function () {
+            document.body.setAttribute("theme", el.dataset.theme)
+         },
+         onEnterBack: function () {
+            document.body.setAttribute("theme", el.dataset.theme)
+         }
+      })
+   })
+
+}
+
+
+
+
+var btns = document.querySelectorAll(".btn")
+
+btns.forEach((btn) => {
+   crsrHov(btn)
 })
 
 
-var tl4= gsap.timeline({
-   scrollTrigger:{
-      trigger: "#quoteSec",
-      scroller: "#main",
-      start: "top 70%",
-      end: "top 00%",
-      scrub:  3,
-   }
-})
-tl4.from("#quoteSec .quote i:nth-child(1)",{
-   x: "-100px",
-   y: "-100px",
-   opacity: 0,
-},"colonAnim")
 
-tl4.from("#quoteSec .quote i:nth-child(3)",{
-   x: "100px",
-   y: "100px",
-   opacity: 0,
-},"colonAnim")
+cursorMoveAnimation()
+autoScroll()
+initialAnimation()
+homeAnimation()
+projectsSecAnimation()
+quoteAnimation();
+themeAnimation()
 
- 
+
+
 
 
 
